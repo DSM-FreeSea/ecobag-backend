7954ded9b3f477cc3b01531a78cdf9fdf72e8238
pjp0704 <junhyeong25@gmail.com>
2021-08-28 21:42:01 +0900

    [FEAT] image upload to AWS S3

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ImagesService } from './images.service';
+import * as multerS3 from 'multer-s3';
+import * as AWS from 'aws-sdk';
 
+const s3 = new AWS.S3();
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+  constructor(private readonly imagesService: ImagesService) {}
+
+  @Post()
+  @UseInterceptors(
+    FilesInterceptor('images', 20, {
+      storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_S3_BUCKET,
+        acl: 'public-read',
+        key: function (req, file, cb) {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  async uploadImage(@UploadedFiles() files: Express.MulterS3.File[]) {
+    return this.imagesService.uploadImage(files);
+  }
+}
