fe0182b6c0a0c2846269d5a2c4135c95635953a2
pjp0704 <junhyeong25@gmail.com>
2021-08-31 11:28:54 +0900

    [ADD] posts resource using nest generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,17 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { ImagesModule } from './images/images.module';
+import { PostsModule } from './posts/posts.module';
 @Module({
   imports: [
-    ImagesModule,
     MongooseModule.forRoot(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
     }),
+    ImagesModule,
+    PostsModule,
   ],
   controllers: [],
   providers: [],
