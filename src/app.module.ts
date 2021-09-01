24afbcb13db3dc5f1a429c565025acb98c2f3aa5
pjp0704 <junhyeong25@gmail.com>
2021-09-01 23:13:21 +0900

    [FEAT] Environment config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { ImagesModule } from './images/images.module';
 import { PostsModule } from './posts/posts.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI, {
       useUnifiedTopology: true,
     }),
