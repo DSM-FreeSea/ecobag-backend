a7a9f4596c8f4a677f7c613a9e9bfd66f4dc9e1f
pjp0704 <junhyeong25@gmail.com>
2021-09-02 01:36:44 +0900

    [REFACTOR] image upload to s3(post included image)

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -6,28 +6,14 @@ import {
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { ImagesService } from './images.service';
-import * as multerS3 from 'multer-s3';
-import * as AWS from 'aws-sdk';
 
-const s3 = new AWS.S3();
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
   @Post()
-  @UseInterceptors(
-    FilesInterceptor('images', 20, {
-      storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET,
-        acl: 'public-read',
-        key: function (req, file, cb) {
-          cb(null, file.originalname);
-        },
-      }),
-    }),
-  )
+  @UseInterceptors(FilesInterceptor('images'))
   async uploadImage(@UploadedFiles() files: Express.MulterS3.File[]) {
-    return this.imagesService.uploadImage(files);
+    return this.imagesService.uploadImage(files, 'upload');
   }
 }
