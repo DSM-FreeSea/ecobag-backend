e361421fcfdb4ed1efdc2a38ee5135a35bd2fd2d
pjp0704 <junhyeong25@gmail.com>
2021-09-02 15:36:48 +0900

    [ADD] export UsersService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -11,5 +11,6 @@ import { UsersRepository } from './users.repository';
   ],
   controllers: [UsersController],
   providers: [UsersRepository, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
